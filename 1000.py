bd3f5a6cf6eb00914e6d612a2c287f8d7a0f86ae
hihihihihihibyebyebye <hwanggyutae98@gmail.com>
2025-10-24 21:15:14 +0900

    Sol 2577

--- 1000.py
+++ 1000.py
@@ -1,1 +1,9 @@
-print("Hello World!")
+a=[0,0,0,0,0,0,0,0,0,0]
+A=int(input())
+B=int(input())
+C=int(input())
+D=A*B*C
+D=str(D)
+for i in range(len(D)):
+    a[int(D[i])]+=1
+print('n'.join(map(str,a)))
